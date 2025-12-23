05103e4896495a996aa505661643fd68e10f7afe
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2025-12-23 21:45:54 +0100

    chore: remove build artifacts and cache files from repository
    - Remove all .next/ directories (build artifacts)
    - Remove all node_modules/ directories (dependencies)
    - Remove .turbo/ cache files
    - Remove webpack cache files (.pack.gz)
    - These files are generated during build and should not be versioned

--- apps/shell/.next/static/chunks/apps_shell_src_app_page_tsx_1971764e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_cf67a8c2._.js",
-  "static/chunks/node_modules_3bce98ec._.js"
-],
-    source: "dynamic"
-});
